00b80fddce4e9c2801550d6f577e3806bb6bf390
bridgetp24 <bridgetpeterkin@gmail.com>
2022-03-24 12:59:13 -0500

    Initial Commit

---
+++ Under the Sea/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Under the Sea
+//
+//  Created by Bridget Peterkin on 3/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Under the Sea/Under_the_SeaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Under_the_SeaApp.swift
+//  Under the Sea
+//
+//  Created by Bridget Peterkin on 3/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Under_the_SeaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
