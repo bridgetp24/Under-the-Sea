48cdae58fd8ff327f2e008716aff29bc1be9276f
bridgetp24 <bridgetpeterkin@gmail.com>
2022-03-31 13:51:11 -0500

    Add bongo functionality

--- Under the Sea/DrumKitTab.swift
+++ Under the Sea/DrumKitTab.swift
@@ -7,16 +7,55 @@
 
 import Foundation
 import SwiftUI
-
+/** playable bongos*/
 struct DrumKitTab: View {
+    let midiModule = MIDIModule(defaultPatch: 0, defaultChannel: 9, midiFileName: "", midiFileExt: "")
     var body: some View {
+        ZStack {
+            VStack {
+                let pitch = 61
+                Text("Bongos")
+                    .font(.title)
+                    .foregroundColor(Color.black)
+
+
         
-        VStack {
-            Text("Todo: Bridget")
-                .font(.title)
-                .foregroundColor(Color.black)
-  
-        }
+                ZStack {
+                    Spacer()
+
+                    Image("bongos2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                    VStack() {
+                        HStack {
+                            NoteButton(text: "High bongo", midiModule: midiModule, pitch: UInt8(pitch - 1), duration: 1, channel: 9)
+                                .padding(.leading, 0)
+                                .frame(width: 125.0, height: 50.0)
+                                .scaleEffect(1)
+                                .foregroundColor(Color.black)
+                            
+                            Spacer()
+                            NoteButton(text: "Low bongo", midiModule: midiModule, pitch: UInt8(pitch), duration: 1, channel: 9)
+                                .padding()
+                                .frame(width: 150.0, height: 50.0)
+                                .scaleEffect(1)
+                                .foregroundColor(Color.black)
+                            
+                        }.frame(width: 300, height: 50)
+                        Spacer()
+                    }.frame(width: 300, height: 190)
+
+
+                    
+                        
+                }
+                .frame(width: 300, height: 190)
+                
+            }
+        }.background(Color.blue)
+
 
     }
+
 }
+
