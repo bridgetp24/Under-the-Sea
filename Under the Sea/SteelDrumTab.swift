c278889552e4fef339652afad2dd29df84f9b359
driedel152 <45524258+driedel152@users.noreply.github.com>
2022-03-30 20:12:56 -0500

    Added all steel drum notes

--- Under the Sea/SteelDrumTab.swift
+++ Under the Sea/SteelDrumTab.swift
@@ -19,33 +19,73 @@ struct SteelDrumsTab: View {
                 Image("SteelPanTransparent")
                     .resizable()
                     .scaledToFit()
-                NoteButton(text: "C", midiModule: midiModule, pitch: 60, duration: 1, channel: 0)
-                    .scaleEffect(3)
-                    .offset(x: 50, y: 250)
-                NoteButton(text: "D", midiModule: midiModule, pitch: 62, duration: 1, channel: 0)
-                    .scaleEffect(3)
-                    .offset(x: 250, y: 100)
-                NoteButton(text: "E", midiModule: midiModule, pitch: 64, duration: 1, channel: 0)
-                    .scaleEffect(3)
-                    .offset(x: 200, y: -150)
-                NoteButton(text: "F", midiModule: midiModule, pitch: 65, duration: 1, channel: 0)
-                    .scaleEffect(3)
-                    .offset(x: -100, y: 250)
-                NoteButton(text: "G", midiModule: midiModule, pitch: 67, duration: 1, channel: 0)
-                    .scaleEffect(3)
-                    .offset(x: 200, y: 200)
-                NoteButton(text: "A", midiModule: midiModule, pitch: 69, duration: 1, channel: 0)
-                    .scaleEffect(3)
-                    .offset(x: 250, y: -50)
-                NoteButton(text: "B", midiModule: midiModule, pitch: 71, duration: 1, channel: 0)
-                    .scaleEffect(3)
-                    .offset(x: 100, y: -250)
-                NoteButton(text: "C", midiModule: midiModule, pitch: 72, duration: 1, channel: 0)
-                    .scaleEffect(3)
-                    .offset(x: 50, y: 150)
+                // Outer notes
+                ForEach(0..<12) {i in
+                    let pitch = i + 60
+                    let angle = -Double(i+3) * 7 * Double.pi/6
+                    NoteButton(text: pitchToText(pitch: pitch), midiModule: midiModule, pitch: UInt8(pitch), duration: 1, channel: 0)
+                        .scaleEffect(3)
+                        .offset(x: cos(angle) * 275, y: sin(angle) * 275)
+                }
+                // Middle notes
+                ForEach(0..<12) {i in
+                    let pitch = i + 72
+                    let angle = -Double(i+3) * 7 * Double.pi/6
+                    NoteButton(text: pitchToText(pitch: pitch), midiModule: midiModule, pitch: UInt8(pitch), duration: 1, channel: 0)
+                        .scaleEffect(2)
+                        .offset(x: cos(angle) * 150, y: sin(angle) * 150)
+                }
+                // Inner notes
+                ForEach(0..<5) {i in
+                    let pitch = i + 84
+                    let angle = Double(i+3) * 4 * Double.pi/5
+                    NoteButton(text: pitchToText(pitch: pitch), midiModule: midiModule, pitch: UInt8(pitch), duration: 1, channel: 0)
+                        .scaleEffect(1.5)
+                        .offset(x: cos(angle) * 50, y: sin(angle) * 50)
+                }
             }
         }
-
+    }
+    
+    func pitchToText(pitch: Int) -> String {
+        if(pitch % 12 == 0) {
+            return "C"
+        }
+        if(pitch % 12 == 1) {
+            return "C#"
+        }
+        if(pitch % 12 == 2) {
+            return "D"
+        }
+        if(pitch % 12 == 3) {
+            return "Eb"
+        }
+        if(pitch % 12 == 4) {
+            return "E"
+        }
+        if(pitch % 12 == 5) {
+            return "F"
+        }
+        if(pitch % 12 == 6) {
+            return "F#"
+        }
+        if(pitch % 12 == 7) {
+            return "G"
+        }
+        if(pitch % 12 == 8) {
+            return "Ab"
+        }
+        if(pitch % 12 == 9) {
+            return "A"
+        }
+        if(pitch % 12 == 10) {
+            return "Bb"
+        }
+        if(pitch % 12 == 11) {
+            return "B"
+        }
+        
+        return "Z"
     }
 }
 
